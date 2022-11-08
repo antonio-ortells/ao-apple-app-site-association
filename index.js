5de6d2dcbf77f96e68a39c74ec2b7e453b7d473f
Antonio Ortells <antonio.ortells@okta.com>
2022-11-08 09:29:58 +0100

    Extended functions

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var Webtask = require('webtask-tools');
+var request = require('request');
 
 var app = express();
-const port = 3000
+const port = 3000;
+
+const BASEURL='http://echo.jsontest.com';
 
 app.get('/', function (req, res) {
-  res.status(200).send('Hello World');
+  res.status(200).send(
+    {
+      applinks: {
+        apps: [],
+        details: [
+          {
+            appID: "9JA89QQLNQ.com.my.auth.bundle",
+            paths: ["/ios/com.my.auth.bundle/*"]
+          }
+        ]
+      }
+    }
+  );
+});
+
+app.get('/some.file', function (req, res) {
+  res.status(200).send("<head></head><body><div><h1>This is some other file</h1></div></body>");
+});
+
+app.get('/*', function (req, res) {
+  var newurl = `${BASEURL}${req.url}`;
+  request(newurl).pipe(res);
 });
 
 app.listen(port, () => {
