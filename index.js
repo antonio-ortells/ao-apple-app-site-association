d61564937b557c3cd00b805534fb2edcdf26b0c2
Antonio Ortells <antonio.ortells@okta.com>
2022-11-09 12:39:30 +0100

    Add logging

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var request = require('request');
 var app = express();
 const port = 3000;
 
-const BASEURL='http://echo.jsontest.com';
+const BASEURL='http://dev-antonio-ortells.eu.auth0.com';
 
 app.get('/', function (req, res) {
+  console.log('Requested root')
   res.status(200).send(
     {
       applinks: {
@@ -23,19 +24,14 @@ app.get('/', function (req, res) {
   );
 });
 
-app.get('/some.file', function (req, res) {
-  res.status(200).send("<head></head><body><div><h1>This is some other file</h1></div></body>");
-});
-
 app.get('/*', function (req, res) {
+  console.log(`Requested ${req.url}`)
   var newurl = `${BASEURL}${req.url}`;
   res.redirect(newurl);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Apple App Site Association listening on port ${port}`)
 })
-
-//module.exports = app;
 
 module.exports = Webtask.fromExpress(app);
