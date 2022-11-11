24b9a18a52383fc945d292277b41734b703d201c
Antonio Ortells <antonio.ortells@okta.com>
2022-11-11 12:37:50 +0100

    Use URL stripping the root path

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/*', function (req, res) {
-  console.log(`Requested ${req.originalUrl}`)
-  var newurl = `${BASEURL}${req.originalUrl}`;
+  console.log(`Requested ${req.url}`)
+  var newurl = `${BASEURL}${req.url}`;
   res.redirect(newurl);
 });
 
